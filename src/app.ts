e2e1ce4f19c4d38676d391fca35656eb6bac45b3
ArafatSany7 <human.sany7@gmail.com>
2026-07-06 17:28:50 +0600

    prisma schema added & error handler added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
 
@@ -13,5 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req: Request, res: Response) => {
   res.send('FixItNow API is running successfully!');
 });
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,32 @@
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+import ApiError from './ApiError';
+
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = 500;
+  let message = 'Something went wrong!';
+  let errorDetails: any = err;
+
+  if (err instanceof ZodError) {
+    statusCode = 400;
+    message = 'Validation Error';
+    errorDetails = err.issues.map((issue) => ({
+      field: issue.path[issue.path.length - 1],
+      message: issue.message,
+    }));
+  } else if (err instanceof ApiError) {
+    statusCode = err?.statusCode;
+    message = err.message;
+  } else if (err instanceof Error) {
+    message = err?.message;
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorDetails,
+    stack: process.env.NODE_ENV === 'development' ? err?.stack : undefined,
+  });
+};
+
+export default globalErrorHandler;
